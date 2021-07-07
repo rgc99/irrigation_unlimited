372b03819687e08a253c9eae8f10234cb51045de
Robert Cook <rc@bigpond.net.au>
2021-07-07 23:25:02 +0000

    Check events vs results

--- tests/test_service.py
+++ tests/test_service.py
@@ -85,12 +85,17 @@ async def run_test(
 
 
 def check_summary(full_path: str, coordinator: IUCoordinator):
-    assert coordinator.tester.total_checks == coordinator.tester.total_results
+    assert (
+        coordinator.tester.total_events
+        == coordinator.tester.total_checks
+        == coordinator.tester.total_results
+    )
     assert coordinator.tester.total_errors == 0
     print(
-        "Finished: {0}; tests: {1}; checks: {2}; errors: {3}; time: {4:.2f}s".format(
+        "Finished: {0}; tests: {1}; events: {2}; checks: {3}; errors: {4}; time: {5:.2f}s".format(
             full_path,
             coordinator.tester.total_tests,
+            coordinator.tester.total_events,
             coordinator.tester.total_checks,
             coordinator.tester.total_errors,
             coordinator.tester.total_time,
