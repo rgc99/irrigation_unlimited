d223cbd6b087b0cf84e6ad471cf665a4cf4d595a
Robert Cook <rc@bigpond.net.au>
2021-07-14 22:30:47 +0000

    Test for empty controllers or zones

---
+++ tests/test_init.py
@@ -0,0 +1,28 @@
+"""Test irrigation_unlimited timing operations."""
+import pytest
+import voluptuous as vol
+import homeassistant.core as ha
+
+from custom_components.irrigation_unlimited.const import (
+    DOMAIN,
+    CONF_CONTROLLERS,
+    CONF_ZONES,
+)
+from custom_components.irrigation_unlimited.__init__ import CONFIG_SCHEMA
+
+
+test_config_dir = "tests/configs/"
+
+MOCK_CONFIG_BAD_1 = {DOMAIN: {CONF_CONTROLLERS: []}}
+MOCK_CONFIG_BAD_2 = {DOMAIN: {CONF_CONTROLLERS: [{CONF_ZONES: []}]}}
+
+
+async def test_init(hass: ha.HomeAssistant):
+    """Test init module."""
+
+    with pytest.raises(vol.MultipleInvalid, match="Must have at least one entry for*"):
+        # No controller configured
+        CONFIG_SCHEMA(MOCK_CONFIG_BAD_1)
+
+        # No zone configured
+        CONFIG_SCHEMA(MOCK_CONFIG_BAD_2)
