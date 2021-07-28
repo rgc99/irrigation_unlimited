c98c2d6014dc82a3f0c58d447f1cfa322b267269
Robert Cook <rc@bigpond.net.au>
2021-07-28 21:39:29 +0000

    Create support module

--- tests/test_service.py
+++ tests/test_service.py
@@ -9,6 +9,7 @@ from homeassistant.config import load_yaml_config_file
 from homeassistant.setup import async_setup_component
 from homeassistant.const import SERVICE_RELOAD
 from tests.const import MOCK_CONFIG
+from tests.iu_test_support import test_config_dir, check_summary
 from custom_components.irrigation_unlimited.irrigation_unlimited import (
     IUCoordinator,
 )
@@ -87,29 +88,6 @@ async def run_test(
     await finish_test(hass, coordinator, next_time, block)
 
     return
-
-
-def check_summary(full_path: str, coordinator: IUCoordinator):
-    assert (
-        coordinator.tester.total_events
-        == coordinator.tester.total_checks
-        == coordinator.tester.total_results
-    )
-    assert coordinator.tester.total_errors == 0
-    print(
-        "Finished: {0}; tests: {1}; events: {2}; checks: {3}; errors: {4}; time: {5:.2f}s".format(
-            full_path,
-            coordinator.tester.total_tests,
-            coordinator.tester.total_events,
-            coordinator.tester.total_checks,
-            coordinator.tester.total_errors,
-            coordinator.tester.total_time,
-        )
-    )
-    return
-
-
-test_config_dir = "tests/configs/"
 
 
 async def test_service_adjust_time(
--- tests/test_timing.py
+++ tests/test_timing.py
@@ -7,8 +7,7 @@ from homeassistant.config import (
     load_yaml_config_file,
     async_process_ha_core_config,
 )
-from homeassistant.setup import async_setup_component
-
+from tests.iu_test_support import test_config_dir, check_summary
 from custom_components.irrigation_unlimited.irrigation_unlimited import (
     IUCoordinator,
 )
@@ -23,30 +22,6 @@ logging.getLogger("homeassistant.components.recorder").setLevel(logging.WARNING)
 logging.getLogger("pytest_homeassistant_custom_component.common").setLevel(
     logging.WARNING
 )
-
-
-def check_summary(full_path: str, coordinator: IUCoordinator):
-    assert (
-        coordinator.tester.total_events
-        == coordinator.tester.total_checks
-        == coordinator.tester.total_results
-    )
-    assert coordinator.tester.total_errors == 0
-    print(
-        "Finished: {0}; tests: {1}; events: {2}; checks: {3}; errors: {4}; time: {5:.2f}s".format(
-            full_path,
-            coordinator.tester.total_tests,
-            coordinator.tester.total_events,
-            coordinator.tester.total_checks,
-            coordinator.tester.total_errors,
-            coordinator.tester.total_time,
-        )
-    )
-    return
-
-
-test_config_dir = "tests/configs/"
-
 
 async def test_timings(hass: ha.HomeAssistant, skip_setup):
     """Test timings. Process all the configuration files in the
