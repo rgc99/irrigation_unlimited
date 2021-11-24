1f7a1debcc2e5f0b4463148be9e1d4dd5e3a530c
rgc99 <rgc99@users.noreply.github.com>
2021-11-24 06:49:25 +0000

    Add missing history file

---
+++ custom_components/irrigation_unlimited/history.py
@@ -0,0 +1,169 @@
+"""History access and caching"""
+from datetime import datetime, timedelta
+from typing import OrderedDict
+import homeassistant.util.dt as dt
+from homeassistant.components.recorder import history
+from homeassistant.const import STATE_ON
+
+from .const import (
+    CONF_HISTORY_REFRESH,
+    CONF_HISTORY_SPAN,
+    JSON_START,
+    JSON_END,
+)
+
+TIMELINE = "timeline"
+TODAY_ON = "today_on"
+
+
+def midnight(utc: datetime) -> datetime:
+    """Accept a UTC time and return midnight for that day"""
+    return dt.as_utc(
+        dt.as_local(utc).replace(hour=0, minute=0, second=0, microsecond=0)
+    )
+
+
+def round_seconds_dt(atime: datetime) -> datetime:
+    """Round the time to the nearest second"""
+    return (atime + timedelta(seconds=0.5)).replace(microsecond=0)
+
+
+def round_seconds_td(duration: timedelta) -> timedelta:
+    """Round the timedelta to the nearest second"""
+    return timedelta(seconds=round(duration.total_seconds()))
+
+
+class IUHistory:
+    """History access and caching"""
+
+    def __init__(self, hass):
+        self._hass = hass
+        # Configuration variables
+        self._history_span: timedelta = None
+        self._history_refresh: timedelta = None
+        # Private variables
+        self._history_last: datetime = None
+        self._cache = {}
+
+    def _clear_cache(self) -> None:
+        self._cache = {}
+
+    def _today_duration(self, stime: datetime, data: list) -> timedelta:
+        """Return the total on time"""
+        # pylint: disable=no-self-use
+        elapsed = timedelta(0)
+        current_state: str = None
+        current_time: datetime = None
+        start = midnight(stime)
+
+        for item in data:
+
+            # Filter data
+            if item.last_changed < start:
+                continue
+            if item.last_changed > stime:
+                break
+
+            # Initialise on first pass
+            if current_state is None:
+                current_state = item.state
+                current_time = item.last_changed
+                continue
+
+            if current_state == STATE_ON and item.state != STATE_ON:
+                elapsed += item.last_changed - current_time
+
+            current_state = item.state
+            current_time = item.last_changed
+
+        if current_state == STATE_ON:
+            elapsed += stime - current_time
+
+        return timedelta(seconds=round(elapsed.total_seconds()))
+
+    def _run_history(self, stime: datetime, data: list) -> list:
+        """Return the on/off series"""
+        # pylint: disable=no-self-use
+        current_state: str = None
+        current_time: datetime = None
+        run_history = []
+
+        for item in data:
+
+            # Initialise on first pass
+            if current_state is None:
+                current_state = item.state
+                current_time = item.last_changed
+                continue
+
+            if current_state == STATE_ON and item.state != STATE_ON:
+                hist = OrderedDict()
+                hist[JSON_START] = round_seconds_dt(current_time)
+                hist[JSON_END] = round_seconds_dt(item.last_changed)
+                run_history.append(hist)
+
+            current_state = item.state
+            current_time = item.last_changed
+
+        if current_state == STATE_ON:
+            hist = OrderedDict()
+            hist[JSON_START] = round_seconds_dt(current_time)
+            hist[JSON_END] = round_seconds_dt(stime)
+            run_history.append(hist)
+
+        return run_history
+
+    def load(self, config: OrderedDict) -> "IUHistory":
+        """Load config data"""
+        if config is None:
+            config = {}
+        self._history_span = timedelta(days=config.get(CONF_HISTORY_SPAN, 7))
+        self._history_refresh = timedelta(seconds=config.get(CONF_HISTORY_REFRESH, 120))
+        return self
+
+    def muster(self, stime: datetime, entity_ids: list[str], force: bool) -> None:
+        """Check and update history as required"""
+        if not (
+            force
+            or self._history_last is None
+            or stime - self._history_last >= self._history_refresh
+            or dt.as_local(self._history_last).toordinal()
+            != dt.as_local(stime).toordinal()
+        ):
+            return
+
+        self._clear_cache()
+        start = stime - self._history_span
+        data = history.get_significant_states(
+            self._hass,
+            start_time=start,
+            end_time=stime,
+            entity_ids=entity_ids,
+            significant_changes_only=False,
+        )
+        self._history_last = stime
+        if data is None or len(data) == 0:
+            return
+
+        for entity_id in data:
+            self._cache[entity_id] = {}
+            self._cache[entity_id][TIMELINE] = self._run_history(stime, data[entity_id])
+            self._cache[entity_id][TODAY_ON] = self._today_duration(
+                stime, data[entity_id]
+            )
+        return
+
+    def today_total(self, entity_id: str) -> timedelta:
+        """Return the total on time for today"""
+        if entity_id in self._cache:
+            return self._cache[entity_id][TODAY_ON]
+        return timedelta(0)
+
+    def timeline(self, entity_id: str) -> list:
+        """Return the timeline history"""
+        if entity_id in self._cache:
+            return self._cache[entity_id][TIMELINE]
+        return []
+
+    def dummy(self):
+        return None
