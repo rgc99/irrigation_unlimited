d300ef2a5175994c7b3fa5273e7cdefec83a314c
rgc99 <rgc99@users.noreply.github.com>
2023-03-12 00:55:08 +0000

    Extend cron testing

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -1,6 +1,10 @@
 """Test irrigation_unlimited cron time interface"""
 # pylint: disable=unused-import
+from unittest.mock import patch
 import homeassistant.core as ha
+from custom_components.irrigation_unlimited.irrigation_unlimited import (
+    IULogger,
+)
 from tests.iu_test_support import IUExam
 
 IUExam.quiet_mode()
@@ -15,5 +19,10 @@ async def test_cron(hass: ha.HomeAssistant, skip_history):
         exam.check_summary()
 
     async with IUExam(hass, "test_cron_error.yaml") as exam:
-        await exam.run_all()
-        exam.check_summary()
+        with patch.object(IULogger, "_format") as mock:
+            await exam.run_all()
+            exam.check_summary()
+
+            assert (
+                sum([1 for call in mock.call_args_list if call.args[1] == "CRON"]) == 2
+            )
