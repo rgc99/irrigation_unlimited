71050918340b3a8134cb4cd5fcb785c0ca6fa100
rgc99 <rgc99@users.noreply.github.com>
2023-09-29 03:39:57 +0000

    Minor tweeks to tests

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -5,11 +5,11 @@ from tests.iu_test_support import IUExam
 
 IUExam.quiet_mode()
 
+# pylint: disable=unused-argument
+
 
 async def test_schedule(hass: ha.HomeAssistant, skip_dependencies, skip_history):
     """Test schedule functionality."""
-    # pylint: disable=unused-argument
-
     async with IUExam(hass, "test_schedule.yaml") as exam:
         await exam.run_all()
         exam.check_summary()
